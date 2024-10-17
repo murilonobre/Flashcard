9670a40f43ee7d15cc4d16c14ee0059a762ecb7b
Lucas <126820432+murilonobre@users.noreply.github.com>
2024-10-17 07:36:16 -0300

    salvei

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criarCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de Programação'
+)
+
+criarCartao(
+    'Geografia',
+    'Qual a capital da França',
+    'A capital da França é Paris'    
+)
+
+criarCartao(
+    'Programação'
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criarCartao(
+    'Lingua Inglesa',
+    'Como se diz oi em Ingles',
+    'Oi em Ingles é HI (RAI)'
+)
